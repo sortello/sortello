0d259661c69262324d1d75689a5d5e93ecee0730
mazzcris <cristiano.mazzarini@gmail.com>
2017-02-02 23:28:18 +0100

    Provisional commit

--- model/treeNodeFactory.spec.js
+++ model/treeNodeFactory.spec.js
@@ -1,9 +1,28 @@
 import treeNodeFactory from "./treeNodeFactory"
+import traverseTree from "./traverseTree"
 
 describe('treeNodeFactory',() => {
     it('should set the correct value to the node', () => {
         const VALUE = "THIS IS SPARTA"
         const result = treeNodeFactory(VALUE)
         expect(result.value).toBe(VALUE)
+    })
+
+    it('should count the nodes of the tree', () => {
+      var nodeOne = treeNodeFactory('node one');
+      var nodeTwo = treeNodeFactory('node two');
+      var nodeThree = treeNodeFactory('node three');
+      var nodeFour = treeNodeFactory('node four');
+      var nodeFive = treeNodeFactory('node five');
+
+      var rootNode = treeNodeFactory('root node');
+      nodeFour.right = nodeFive;
+      nodeThree.left = nodeFour;
+      nodeOne.left = nodeThree;
+      nodeOne.right = nodeTwo;
+      rootNode.left = nodeOne;
+
+      expect(traverseTree(rootNode).length).toBe(6);
+
     })
 })
