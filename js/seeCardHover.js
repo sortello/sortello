33ff7f1392905ac7a0f9860dbb3288748cb3470b
Filippo Gianessi <gianessi.filippo@gmail.com>
2017-02-24 16:17:21 +0100

    Added a JS file for hovering “see card” button (not working)

---
+++ js/seeCardHover.js
@@ -0,0 +1,14 @@
+
+
+
+
+jQuery(document).ready(function(){
+
+  $('choices--button').onmouseover = function() {
+
+      $('button--on-white').addClass('button--on-white-hover');
+      $('button--on-white-hover').removeClass('button--on-white');
+
+  };
+
+});
