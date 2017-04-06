558f7723c503c23911102e0c231107d5c85587a2
fain182 <fain182@gmail.com>
2017-04-06 21:55:06 +0200

    Refactored and improved deploy script
    Every deploy now compile and commit a production build updated.

--- deploy.js
+++ deploy.js
@@ -1,12 +1,26 @@
 var exec = require('child_process').exec;
 
+var arguments = process.argv.slice(2);
+
+if (arguments.length < 2) {
+  console.log("Usage: node deploy.js <version> <message>");
+  return 1;
+}
+
 var version = process.argv.slice(2)[0];
 var message = process.argv.slice(2)[1];
 
-console.log(version);
-console.log(message);
 
-var cmd = 'git checkout master && git pull && npm run build && git tag -a ' + version + ' -m "' + message + '" && git push --tags && git checkout gh-pages && git merge master && npm test && git push origin gh-pages && git checkout master';
+
+console.log("Release " + version + ":" + message);
+
+var getMasterUpdated = 'git checkout master && git pull && ';
+var compileJsForProduction = '  npm run build && git commit -m "update js production build"';
+var tagNewRelease = 'git tag -a ' + version + ' -m "' + message + '" && git push --tags && ';
+var pushToGithubPages = 'git checkout gh-pages && git merge master && npm test && git push origin gh-pages && ';
+var backToMasterBranch = 'git checkout master';
+
+var cmd = getMasterUpdated +  compileJsForProduction + tagNewRelease + pushToGithubPages + backToMasterBranch;
 
 console.log("Command: " + cmd);
 
