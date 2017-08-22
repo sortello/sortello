c87e43d66b74f8c8b05f4b69532089a5530095c6
Filippo Gianessi <gianessi.filippo@gmail.com>
2017-08-22 12:51:30 +0200

    Defined style for Recap.jsx + Updated UI + Added sortello screenshots and icons

--- components/Recap.jsx
+++ components/Recap.jsx
@@ -3,9 +3,9 @@ import React from "react"
 const Recap = React.createClass({
     render: function() {
         return (
-			<div className="order-recap">
+			<div className="order-recap recap__container">
         {this.props.cards.map((item, index) => (
-					<p key={index}>{item.value.name}</p>
+					<div className="recap__item" key={index}>{item.value.name}</div>
         ))}
 			</div>
         );
