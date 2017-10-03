adf5269c6a7a50bae92a68ef5272e010bdc96762
mazzcris <cristiano.mazzarini@gmail.com>
2017-10-03 23:19:51 +0200

    Merge UI-restyle branch, fix expectRecap.js

--- tests/protractor/expectRecap.js
+++ tests/protractor/expectRecap.js
@@ -1,8 +1,13 @@
 module.exports.toBe = function (recapArray) {
-  let recapDiv = element(by.css('div.order-recap'));
+  let showRecapButton = element(by.css('.trigger-button__link'));
   let EC = protractor.ExpectedConditions;
-  browser.wait(EC.visibilityOf(recapDiv), 20000).then(function () {
-    let recap = element(by.css('div.order-recap')).all(by.css('p'));
-    expect(recap.getText()).toEqual(recapArray);
+  browser.wait(EC.presenceOf(showRecapButton), 20000).then(function () {
+    element(by.css('.trigger-button__link')).click();
+    let recapDiv = element(by.css('div.order-recap'));
+    browser.wait(EC.presenceOf(recapDiv), 20000).then(function () {
+      let recap = element.all(by.css('div.order-recap div.recap__item'));
+      expect(recap.getText()).toEqual(recapArray);
+    });
   });
 }
+
