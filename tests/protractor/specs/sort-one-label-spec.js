573b69263176bb1ab3186c6dd925a58972198de4
mazzcris <cristiano.mazzarini@gmail.com>
2017-10-14 23:20:07 +0200

    Add sort-one-label-spec

---
+++ tests/protractor/specs/sort-one-label-spec.js
@@ -0,0 +1,43 @@
+describe('sort list asc', function () {
+  it('prioritizes the test column using undo button', function () {
+      browser.ignoreSynchronization = true;
+      protractor.accessFromChromeExtension.accessFromChromeExtension();
+      let EC = protractor.ExpectedConditions;
+
+      function nextChoice () {
+
+        let leftCard = element(by.css('#left_button .card__title'));
+        let rightCard = element(by.css('#right_button .card__title'));
+
+        browser.wait(EC.and(EC.presenceOf(leftCard), EC.presenceOf(rightCard)), 20000).then(function () {
+          leftCard.getText().then(function (leftValue) {
+            rightCard.getText().then(function (rightValue) {
+                if (parseInt(rightValue) < parseInt(leftValue)) {
+                  element(by.css('#right_button .container__card')).click()
+                } else {
+                  element(by.css('#left_button .container__card')).click()
+                }
+            });
+          });
+        });
+
+        element.all(by.id("update_board")).count().then(function (size) {
+          if (size == 0) {
+            nextChoice();
+          }
+        });
+      }
+
+      let allLabel = element(by.css('.label.label__blue'));
+      browser.wait(EC.presenceOf(allLabel), 20000).then(function () {
+        allLabel.click();
+        nextChoice();
+      })
+
+
+      protractor.expectRecap.toBe(['3', '4', '6', '10']);
+
+    }
+  );
+
+});
