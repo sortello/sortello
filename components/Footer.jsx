306ead654bbfa079cae5e024d548fa2d78552386
mazzcris <cristiano.mazzarini@gmail.com>
2017-10-15 19:04:21 +0200

    Remove React.createClass in some components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 
-const Footer = React.createClass({
-   
-    render: function() {
-        return (
-			<div className="footer__container">
-    			<a className="footer__item footer__link" href="disclaimer.html" target="blank">Disclaimer</a>
-    			<div className="footer__item">Proudly supported by <a href="//ideato.it" target="_blank" className="footer__link">ideato</a></div>
-			</div>
-        );
-    }
+class Footer extends React.Component {
 
-});
+  render () {
+    return (
+      <div className="footer__container">
+        <a className="footer__item footer__link" href="disclaimer.html" target="blank">Disclaimer</a>
+        <div className="footer__item">Proudly supported by <a href="//ideato.it" target="_blank"
+                                                              className="footer__link">ideato</a></div>
+      </div>
+    );
+  }
+
+};
 
 export default Footer
