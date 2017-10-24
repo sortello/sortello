9601d144789c4b2f0d0a19d6c0dbdeeb3118b999
JasonSooter <jason.sooter@gmail.com>
2017-10-24 00:23:16 -0500

    Added Card Label functionality. Mapped through the labels array on props.data. Added BEMstyles based on Trello color codes.

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,25 +1,54 @@
-import React from "react"
+import React from 'react';
 
-class Card extends React.Component{
-  render () {
+class Card extends React.Component {
+  render() {
     return (
-      <div className={"wrapper__card choices--button"} id={this.props.id}>
-        <div className={"container__card"} onClick={() => {
-          this.props.handleClick(this.props.side)
-        }} data-cardId="0">
-          <div className="card__title">{this.props.data.name}</div>
+      <div
+        className={'wrapper__card choices--button'}
+        id={this.props.id}
+      >
+        <div
+          className={'container__card'}
+          onClick={() => {
+            this.props.handleClick(this.props.side);
+          }}
+          data-cardId="0"
+        >
+          <div className="card__title">
+            {this.props.data.name}
+          </div>
+          <div>
+            {this.props.data.labels.map(label => (
+              <div
+                className={`card__label card__label--${label.color}`}
+                key={label.id}
+              >
+                {label.name}
+              </div>
+            ))}
+          </div>
         </div>
         <div className="wrapper__card-buttons">
-          <a href={this.props.data.shortUrl} target="_blank" role="button"
-             className="button-seecard card-button__see-card" onClick={(e) => {
-            this.linkClicked(e);
-          }}>
-            <div className="button-text__see-card">See card</div>
+          <a
+            href={this.props.data.shortUrl}
+            target="_blank"
+            role="button"
+            className="button-seecard card-button__see-card"
+            onClick={e => {
+              this.linkClicked(e);
+            }}
+          >
+            <div className="button-text__see-card">
+              See card
+            </div>
           </a>
-          <div className="card-button__esclude button-blacklist button-text__esclude"
-               onClick={() => {
-                 this.props.forget(this.props.data.id)
-               }} data-cardid={this.props.data.id}>
+          <div
+            className="card-button__esclude button-blacklist button-text__esclude"
+            onClick={() => {
+              this.props.forget(this.props.data.id);
+            }}
+            data-cardid={this.props.data.id}
+          >
             Forget this card
           </div>
         </div>
@@ -28,4 +57,4 @@ class Card extends React.Component{
   }
 }
 
-export default Card
+export default Card;
