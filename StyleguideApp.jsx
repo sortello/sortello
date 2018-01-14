448b0196f0ee50385f5838453468df008d0d15ba
mazzcris <cristiano.mazzarini@gmail.com>
2018-01-14 09:54:53 +0100

    Add styleguide app

--- StyleguideApp.jsx
+++ StyleguideApp.jsx
@@ -1,6 +1,8 @@
 import React from "react"
-import Results from "./components/Results.jsx"
-
+import PrioritizationEnd from "./components/PrioritizationEnd.jsx"
+import CardsWithVoters from "./styleGuideComponents/CardsWithVoters.jsx"
+import ChoicesVoter from "./components/ChoicesVoter.jsx"
+import ChoicesView from "./components/view/ChoicesView.jsx"
 
 class StyleguideApp extends React.Component {
   constructor (props) {
@@ -8,9 +10,85 @@ class StyleguideApp extends React.Component {
   }
 
   render () {
+
+    let voters = [
+      {trelloAvatar: '//www.gravatar.com/avatar/94d093eda664addd6e450d7ew881bcad?s=32&d=identicon&r=PG'},
+      {trelloAvatar: '//www.gravatar.com/avatar/94d093eda664addd6e450d7ew831bcad?s=32&d=identicon&r=PG'}
+    ]
+
+    let continueButton = <div className="card-button__continue"
+                              onClick={() => {
+                              }}>Continue</div>
+
+
+    let leftCard = {
+      value: {
+
+        id: 1,
+        shortUrl: "#",
+        name: "Right card with some text",
+        labels: [
+          {
+            color: "red",
+            name: "Label",
+            id: 3
+          },
+          {
+            color: "blue",
+            name: "Label",
+            id: 4
+          }
+        ]
+      }
+    }
+    let rightCard = {
+      value: {
+
+        id: 2,
+        shortUrl: "#",
+        name: "Right card with some text",
+        labels: [
+          {
+            color: "red",
+            name: "Label",
+            id: 3
+          },
+          {
+            color: "blue",
+            name: "Label",
+            id: 4
+          }
+        ]
+      }
+    }
     return (
       <div id="container_div">
-        <Results rootNode={this.state.rootNode} Trello={{}}/>
+        <CardsWithVoters voters={[]} continueButton={""}/>
+        <hr/>
+        <CardsWithVoters voters={voters} continueButton={continueButton}/>
+        <hr/>
+        <ChoicesVoter/>
+        <hr/>
+        <PrioritizationEnd/>
+        <hr/>
+        <h1>Choices view for voter</h1>
+        <ChoicesView
+          roomLink={null}
+          newRoomButton={""}
+          leftContinueButton={""}
+          rightContinueButton={""}
+          roomVoters={[]}
+          everybodyVoted={false}
+          leftCard={leftCard}
+          rightCard={rightCard}
+          leftVoters={[]}
+          rightVoters={[]}
+          handleAddToBlacklist={null}
+          handleUndoClicked={null}
+          handleCardClicked={null}
+          progress={0}
+        />
+
       </div>
     )
   }
