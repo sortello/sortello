bd182d93b412758c57d3c12a74e893a1abcce021
mazzcris <cristiano.mazzarini@gmail.com>
2018-01-15 20:44:01 +0100

    Refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,37 @@
-const path = require('path')
+const loaders = [{
+  test: /\.jsx?$/,         // Match both .js and .jsx files
+  exclude: /node_modules/,
+  loader: "babel",
+  query:
+    {
+      presets: ["react", "es2015"]
+    }
+},
+  {
+    test: /\.css$/,
+    loader: "style-loader!css-loader"
+  }, {
+    test: /\.png$/,
+    loader: "url-loader?limit=100000"
+  }, {
+    test: /\.jpg$/,
+    loader: "file-loader"
+  }, {
+    test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+    loader: 'url?limit=10000&mimetype=application/font-woff'
+  }, {
+    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+    loader: 'url?limit=10000&mimetype=application/octet-stream'
+  }, {
+    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+    loader: 'file'
+  }, {
+    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+    loader: 'url?limit=10000&mimetype=image/svg+xml'
+  }
+]
 
 module.exports = [{
-
   entry: "./index.js",
   output: {
     path: './build',       // target dir
@@ -9,38 +39,7 @@ module.exports = [{
     publicPath: "/build/" // path in URL
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,         // Match both .js and .jsx files
-      exclude: /node_modules/,
-      loader: "babel",
-      query:
-        {
-          presets: ["react", "es2015"]
-        }
-    },
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      }, {
-        test: /\.png$/,
-        loader: "url-loader?limit=100000"
-      }, {
-        test: /\.jpg$/,
-        loader: "file-loader"
-      }, {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      }, {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      }, {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
-      }, {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      }
-    ]
+    loaders: loaders
   }
 },
   {
@@ -51,37 +50,6 @@ module.exports = [{
       publicPath: "/styleguideBuild/" // path in URL
     },
     module: {
-      loaders: [{
-        test: /\.jsx?$/,         // Match both .js and .jsx files
-        exclude: /node_modules/,
-        loader: "babel",
-        query:
-          {
-            presets: ["react", "es2015"]
-          }
-      },
-        {
-          test: /\.css$/,
-          loader: "style-loader!css-loader"
-        }, {
-          test: /\.png$/,
-          loader: "url-loader?limit=100000"
-        }, {
-          test: /\.jpg$/,
-          loader: "file-loader"
-        }, {
-          test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-          loader: 'url?limit=10000&mimetype=application/font-woff'
-        }, {
-          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-          loader: 'url?limit=10000&mimetype=application/octet-stream'
-        }, {
-          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-          loader: 'file'
-        }, {
-          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-          loader: 'url?limit=10000&mimetype=image/svg+xml'
-        }
-      ]
+      loaders: loaders
     }
   }]
