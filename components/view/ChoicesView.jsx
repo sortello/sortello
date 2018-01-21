632e1c1c4aff9a8cfb3eb737eca4068decb59969
mazzcris <cristiano.mazzarini@gmail.com>
2018-01-21 21:50:18 +0100

    Remove duplication

--- components/view/ChoicesView.jsx
+++ components/view/ChoicesView.jsx
@@ -10,69 +10,71 @@ import {remove} from "lodash"
 
 class ChoicesView extends React.Component {
 
-  constructor (props) {
-    super(props);
-  }
-
-  render () {
-    let undoButton = ''
-    if (this.props.handleUndoClicked) {
-      undoButton =
-        <button onClick={() => this.props.handleUndoClicked()} id="undo_button" className="normalize__undo-button">
-          <div className="undo__button">
-            <div className="undo__icon">
-              <img src="assets/icons/undo-icon.svg" alt=""/>
-              Undo choice
-            </div>
-          </div>
-        </button>
+    constructor (props) {
+        super(props);
+        this.printCard = this.printCard.bind(this)
     }
 
-
-    if (this.props.leftCard == null || this.props.rightCard == null) {
-      return (<div><span>Loading...</span></div>);
+    printCard (id, side, data, voters) {
+        return <Card id={id} side={side} handleClick={this.props.handleCardClicked}
+                     forget={this.props.handleAddToBlacklist} data={data}
+                     voters={voters}
+                     everybodyVoted={this.props.everybodyVoted}
+                     handleGoToNextVoting={this.props.handleGoToNextVoting}
+        />
     }
-    return (
-      <div id="second_div">
-        <div className="container__choose-card">
-          <div className="container__top-bar">
-            <div className="choose-card__heading">Select the highest priority card</div>
-            <div className="container__prioritization-status">
-              <div className={"progressive-bar__status-structure"}>
-                <div className={"progressive-bar__status"} role="progressbar" aria-valuenow={this.props.progress}
-                     aria-valuemin="0"
-                     aria-valuemax="100" style={{width: this.props.progress + '%'}}>
+
+    render () {
+        let undoButton = ''
+        if (this.props.handleUndoClicked) {
+            undoButton =
+                <button onClick={() => this.props.handleUndoClicked()} id="undo_button"
+                        className="normalize__undo-button">
+                    <div className="undo__button">
+                        <div className="undo__icon">
+                            <img src="assets/icons/undo-icon.svg" alt=""/>
+                            Undo choice
+                        </div>
+                    </div>
+                </button>
+        }
+
+
+        if (this.props.leftCard == null || this.props.rightCard == null) {
+            return (<div><span>Loading...</span></div>);
+        }
+        return (
+            <div id="second_div">
+                <div className="container__choose-card">
+                    <div className="container__top-bar">
+                        <div className="choose-card__heading">Select the highest priority card</div>
+                        <div className="container__prioritization-status">
+                            <div className={"progressive-bar__status-structure"}>
+                                <div className={"progressive-bar__status"} role="progressbar"
+                                     aria-valuenow={this.props.progress}
+                                     aria-valuemin="0"
+                                     aria-valuemax="100" style={{width: this.props.progress + '%'}}>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {this.printCard('left_button', 'node', this.props.leftCard.value, this.props.voters.left)}
+                    {this.printCard('right_button', 'compareNode', this.props.rightCard.value, this.props.voters.right)}
+                    {/*<TreeDraw tree={this.state.rootNode}></TreeDraw>*/}
+                    {undoButton}
                 </div>
-              </div>
+                <div className={"footer"}>
+                    <Footer/>
+                    <Header/>
+                </div>
+                {this.props.newRoomButton}
+                {this.props.roomLink}
+                {this.props.roomVoters.map((item, index) => (
+                    <img className={'card__voter'} key={index} src={item.avatar}/>
+                ))}
             </div>
-          </div>
-          <Card id="left_button" side="node" handleClick={this.props.handleCardClicked}
-                forget={this.props.handleAddToBlacklist} data={this.props.leftCard.value}
-                voters={this.props.voters.left}
-                everybodyVoted={this.props.everybodyVoted}
-                handleGoToNextVoting={this.props.handleGoToNextVoting}
-                />
-          <Card id="right_button" side="compareNode" handleClick={this.props.handleCardClicked}
-                forget={this.props.handleAddToBlacklist} data={this.props.rightCard.value}
-                voters={this.props.voters.right}
-                everybodyVoted={this.props.everybodyVoted}
-                handleGoToNextVoting={this.props.handleGoToNextVoting}
-                />
-          {/*<TreeDraw tree={this.state.rootNode}></TreeDraw>*/}
-          {undoButton}
-        </div>
-        <div className={"footer"}>
-          <Footer/>
-          <Header/>
-        </div>
-        {this.props.newRoomButton}
-        {this.props.roomLink}
-        {this.props.roomVoters.map((item, index) => (
-          <img className={'card__voter'} key={index} src={item.avatar}/>
-        ))}
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default ChoicesView
