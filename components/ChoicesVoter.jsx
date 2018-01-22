0ac10c502ee866192d62929da05571066999b3e8
mazzcris <cristiano.mazzarini@gmail.com>
2018-01-22 21:10:44 +0100

    Refactoring

--- components/ChoicesVoter.jsx
+++ components/ChoicesVoter.jsx
@@ -8,141 +8,145 @@ import ChoicesView from './view/ChoicesView.jsx'
 
 let socket = false;
 if (typeof socketAddress !== 'undefined') {
-  if (socketAddress !== null) {
-    socket = io(socketAddress);
-  }
+    if (socketAddress !== null) {
+        socket = io(socketAddress);
+    }
 }
 
 const params = queryString.parse(location.search);
 
 class Choices extends React.Component {
-  constructor (props) {
-    super(props);
-    let component = this
-    this.handleCardClicked = this.handleCardClicked.bind(this)
-    this.Trello = this.props.Trello
-    this.renderForbidden = this.renderForbidden.bind(this)
-    this.renderLoading = this.renderLoading.bind(this)
-    this.room = false;
-    this.state = {
-      leftCard: null,
-      rightCard: null,
-      ended: false,
-      voters: {
-        left: [],
-        right: []
-      },
-      hasVoted: false,
-      hasBoardPermissions: false
-    }
-
-    if (params.roomKey !== undefined) {
-      component.state.roomId = params.roomKey
-      if (socket) {
-        this.room = new Room(socket, params.roomKey);
-        socket.on('connect', function () {
-          component.props.Trello.members.get('me', {}, function (data) {
-            component.trelloId = data.id
-            component.trelloAvatar = '//trello-avatars.s3.amazonaws.com/' + data.avatarHash + '/50.png'
-            if (data.avatarHash === null) {
-              component.trelloAvatar = '//www.gravatar.com/avatar/' + data.gravatarHash + '?s=64&d=identicon'
-            }
-            component.room.join(component.trelloId, component.trelloAvatar)
-            component.room.getBoardId()
-            component.room.getCurrentChoice()
-          }, function (e) {
-            console.log(e);
-          });
-        });
-
-        window.addEventListener("beforeunload", (ev) => {
-          ev.preventDefault()
-          this.room.leave(component.trelloId)
-        });
-      }
-
-    }
-    if (socket) {
-      socket.on('votesInfo', function (leftVoters, rightVoters) {
-        component.setState({
-          voters: {
-            left: leftVoters,
-            right: rightVoters,
-          }
-        })
-      })
-
-      socket.on('castBoardIdToVoters', function (boardId) {
-        component.Trello.boards.get(boardId, function () {
-          component.setState({
-            hasBoardPermissions: true
-          })
-        }, function () {
-          component.setState({
+    constructor (props) {
+        super(props);
+        let component = this
+        this.handleCardClicked = this.handleCardClicked.bind(this)
+        this.Trello = this.props.Trello
+        this.renderForbidden = this.renderForbidden.bind(this)
+        this.renderLoading = this.renderLoading.bind(this)
+        this.room = false;
+        this.state = {
+            leftCard: null,
+            rightCard: null,
+            ended: false,
+            voters: {left: [], right: []},
+            hasVoted: false,
             hasBoardPermissions: false
-          }, function () {
-            component.room.leave(component.trelloId)
-          })
+        }
+        if (params.roomKey !== undefined) {
+            this.setUpRoom(component);
+        }
+        if (socket) {
+            this.setUpSocket(component);
+        }
+    }
+
+    setUpSocket (component) {
+        socket.on('votesInfo', function (leftVoters, rightVoters) {
+            component.setState({
+                voters: {
+                    left: leftVoters,
+                    right: rightVoters,
+                }
+            })
         })
-      })
 
-      socket.on('nextChoice', function (leftCard, rightCard) {
-        component.setState({
-          leftCard: leftCard,
-          rightCard: rightCard,
-          hasVoted: false
+        socket.on('castBoardIdToVoters', function (boardId) {
+            component.Trello.boards.get(boardId, function () {
+                component.setState({
+                    hasBoardPermissions: true
+                })
+            }, function () {
+                component.setState({
+                    hasBoardPermissions: false
+                }, function () {
+                    component.room.leave(component.trelloId)
+                })
+            })
         })
-      })
 
-      socket.on('prioritizationEnded', function () {
-        component.setState({
-          ended: true
+        socket.on('nextChoice', function (leftCard, rightCard) {
+            component.setState({
+                leftCard: leftCard,
+                rightCard: rightCard,
+                hasVoted: false
+            })
         })
-      })
-    }
-  }
 
-  handleCardClicked (side) {
-    if (!this.state.hasVoted && this.room) {
-      this.room.castCardClicked(side, this.trelloId, this.trelloAvatar)
+        socket.on('prioritizationEnded', function () {
+            component.setState({
+                ended: true
+            })
+        })
     }
-    this.setState({
-      hasVoted: true
-    })
-  }
 
-  renderForbidden(){
-      return <div id="forbidden-div">You have no access to this board.</div>
-  }
+    setUpRoom (component) {
+        component.state.roomId = params.roomKey
+        if (socket) {
+            component.room = new Room(socket, params.roomKey);
+            socket.on('connect', function () {
+                component.props.Trello.members.get('me', {}, function (data) {
+                    component.trelloId = data.id
+                    component.trelloAvatar = '//trello-avatars.s3.amazonaws.com/' + data.avatarHash + '/50.png'
+                    if (data.avatarHash === null) {
+                        component.trelloAvatar = '//www.gravatar.com/avatar/' + data.gravatarHash + '?s=64&d=identicon'
+                    }
+                    component.room.join(component.trelloId, component.trelloAvatar)
+                    component.room.getBoardId()
+                    component.room.getCurrentChoice()
+                }, function (e) {
+                    console.log(e);
+                });
+            });
 
-  renderLoading(){
-      return (<span>Loading...</span>);
-  }
+            window.addEventListener("beforeunload", (ev) => {
+                ev.preventDefault()
+                component.room.leave(component.trelloId)
+            });
+        }
+    }
 
-  render () {
-    if (!this.state.hasBoardPermissions) {
-      return this.renderForbidden()
+    handleCardClicked (side) {
+        if (!this.state.hasVoted && this.room) {
+            this.room.castCardClicked(side, this.trelloId, this.trelloAvatar)
+        }
+        this.setState({
+            hasVoted: true
+        })
     }
-    if (this.state.leftCard == null || this.state.rightCard == null) {
-      return this.renderLoading()
+
+    renderForbidden () {
+        return <div id="forbidden-div">You have no access to this board.</div>
     }
-    if (this.state.ended) {
-      return (<PrioritizationEnd/>)
+
+    renderLoading () {
+        return (<span>Loading...</span>);
     }
-    return (
-      <ChoicesView
-        roomVoters={[]}
-        leftCard={this.state.leftCard}
-        rightCard={this.state.rightCard}
-        everybodyVoted={this.state.voters.left.length + this.state.voters.right.length > 0}
-        voters={this.state.voters}
-        handleAddToBlacklist={null}
-        handleCardClicked={this.handleCardClicked}
-        handleGoToNextVoting={() => {}}
-        progress={0}
-      />
-    )
-  }
+
+    render () {
+        if (!this.state.hasBoardPermissions) {
+            return this.renderForbidden()
+        }
+        if (this.state.leftCard == null || this.state.rightCard == null) {
+            return this.renderLoading()
+        }
+        if (this.state.ended) {
+            return (<PrioritizationEnd/>)
+        }
+        return (
+            <ChoicesView
+                roomVoters={[]}
+                leftCard={this.state.leftCard}
+                rightCard={this.state.rightCard}
+                everybodyVoted={this.state.voters.left.length + this.state.voters.right.length > 0}
+                voters={this.state.voters}
+                handleAddToBlacklist={null}
+                handleCardClicked={this.handleCardClicked}
+                handleGoToNextVoting={() => {
+                }}
+                progress={0}
+            />
+        )
+    }
 }
 
 export default Choices
