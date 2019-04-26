9d1ff010cb748b350babe76ee64e7fcc53348bd7
LucaMonda <luca-mondaini@hotmail.it>
2019-04-26 14:50:45 +0200

    Refactoring code

--- tests/protractor/specs/empty-params-url-spec.js
+++ tests/protractor/specs/empty-params-url-spec.js
@@ -38,57 +38,24 @@ describe('cannot vote if params is empty', function () {
                                 link.indexOf("&"),
                                 link.lastIndexOf("&")
                             );
-                            link = link.replace(browser.params.hostname + "/app.html", browser.params.hostname + ":4000/app.html");
-                            link = link.replace(extId,"");
-                            browser2.get(link)
+                            let linkModified = link.replace(browser.params.hostname + "/app.html", browser.params.hostname + ":4000/app.html");
+                            linkModified = linkModified.replace(extId,"");
+                            browser2.get(linkModified)
 
                             browser2.wait(EC.presenceOf(element(by.id('container_div'))), 20000).then(function () {
                                 browser.sleep(2000);
                                 expect(browser2.element(by.id('container_div')).getText()).toMatch(/.*Url isn't correct.*/)
                                 browser2.element.all(by.id("second_div")).count().then(function (count) {
                                     expect(count).toEqual(0)
-                                    browser2.close()
-                                    browser1.close()
-                                    done()
                                 })
                             })
-                        })
-                    })
-                })
-            });
-        });
-    });
-    it('cannot vote if fw is missing', function (done) {
 
-        browser.ignoreSynchronization = true;
-        browser.driver.manage().window().setSize(840, 1032);
-        browser.driver.manage().window().setPosition(0, 0);
-        browser.driver.manage().window().toolbar = 0;
-        browser.driver.manage().window().menubar = 0;
-
-        let browser1 = browser
-
-        let browser2 = browser.forkNewDriverInstance();
-        browser2.ignoreSynchronization = true;
-        browser2.driver.manage().window().setSize(840, 524);
-        browser2.driver.manage().window().setPosition(840, 0);
-
-        protractor.accessFromChromeExtension.accessFromChromeExtension(browser1).then(function () {
-            protractor.accessFromChromeExtension.accessFromChromeExtension(browser2).then(function () {
-                let EC = protractor.ExpectedConditions;
-                browser1.get('/app.html?extId=' + browser.params.testTrelloPrivateBoardExtId);
-                let newRoomButton = element(by.css('#new-room-button'))
-                browser1.wait(EC.presenceOf(newRoomButton), 20000).then(function () {
-                    newRoomButton.click();
-                    let roomLinkElement = element(by.css('#room-link'))
-                    browser1.wait(EC.presenceOf(roomLinkElement), 20000).then(function () {
-                        roomLinkElement.getAttribute('value').then(function (link) {
                             let fw = link.substring(
                                 link.lastIndexOf("&")
                             );
-                            link = link.replace(browser.params.hostname + "/app.html", browser.params.hostname + ":4000/app.html");
-                            link = link.replace(fw,"");
-                            browser2.get(link)
+                            linkModified = link.replace(browser.params.hostname + "/app.html", browser.params.hostname + ":4000/app.html");
+                            linkModified = linkModified.replace(fw,"");
+                            browser2.get(linkModified)
 
                             browser2.wait(EC.presenceOf(element(by.id('container_div'))), 20000).then(function () {
                                 browser.sleep(2000);
@@ -100,6 +67,7 @@ describe('cannot vote if params is empty', function () {
                                     done()
                                 })
                             })
+
                         })
                     })
                 })
