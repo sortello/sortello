1deb9140757e8daa600ea7d820e65e81638958ac
LucaMonda <luca-mondaini@hotmail.it>
2019-04-29 16:59:36 +0200

    Blocked option in the select tag

--- components/Select.jsx
+++ components/Select.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default ({options, onChange, placeHolder}) => {
     return <div className="input-select">
             <select onChange={(event) => onChange(event.target.value)} className={"select-field__select input-select__field"}>
-                <option>{placeHolder}</option>
+                <option selected disabled>{placeHolder}</option>
                 {options}
             </select>
             <i className="material-icons input-select__icon">arrow_right</i>
