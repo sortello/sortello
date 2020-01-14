de5a43d6f3c6b4f7ad1c56cd19884c84bf82d126
Cristiano Mazzarini <cristiano.mazzarini@gmail.com>
2020-01-14 08:28:56 +0100

    Update gitignore and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,8 +6,6 @@ class Footer extends React.Component {
     return (
       <div className="footer__container">
         <a className="footer__item footer__link" href="policy.html" target="blank">Disclaimer</a>
-        <div className="footer__item">Proudly supported by <a href="//ideato.it" target="_blank"
-                                                              className="footer__link">ideato</a></div>
       </div>
     );
   }
